ad4901355773a18dc4ef790229f540928029ff25
Ronson Wuniardy <rwuniard@hotmail.com>
2020-08-29 17:10:13 -0400

    updated

--- src/routers/firstRouter.js
+++ src/routers/firstRouter.js
@@ -3,7 +3,7 @@ const router = new express.Router();
 
 // This is a test rest service.
 router.get('/test', (req, resp) => {
-    resp.send('This is test');
+    resp.send('This is another test 5');
 });
 
 module.exports = router;
