5d1f87e11ce6fee69a6de13b19e04f873f5e4ab8
Ronson Wuniardy <rwuniard@hotmail.com>
2020-09-20 18:05:12 -0400

    updated with clusterip and ingress

---
+++ deploy.sh
@@ -0,0 +1,9 @@
+docker build -t rwuniard/myrestsvc:latest -t rwuniard/multi-client:$SHA -f ./Dockerfile ./
+
+# We have login in .travis.yaml, so we can just push it to dockerhub
+docker push rwuniard/myrestsvc:latest
+docker push rwuniard/myrestsvc:$SHA
+
+# the kubectl has been set in .travis.yaml 
+kubectl apply -f k8s
+kubectl set image deployments/myrest-server-deployment myrestserver=rwuniard/myrestsvc:$SHA
