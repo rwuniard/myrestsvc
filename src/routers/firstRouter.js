e8cd16eb2f343fdb80f52ca1d9ce0277a48c80cb
Ronson Wuniardy <rwuniard@hotmail.com>
2020-09-21 22:01:41 -0400

    update it to 7

--- src/routers/firstRouter.js
+++ src/routers/firstRouter.js
@@ -3,7 +3,7 @@ const router = new express.Router();
 
 // This is a test rest service.
 router.get('/test', (req, resp) => {
-    resp.send('This is another test 6');
+    resp.send('This is another test 7');
 });
 
 module.exports = router;
