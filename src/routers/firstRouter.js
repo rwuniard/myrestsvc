6786700c92d15fcb5bc880bf44312e5481e48152
Ronson Wuniardy <rwuniard@hotmail.com>
2022-06-27 17:00:45 -0400

    Fixing the skaffold. Adding nodemon and the dev op

--- src/routers/firstRouter.js
+++ src/routers/firstRouter.js
@@ -3,7 +3,7 @@ const router = new express.Router();
 
 // This is a test rest service.
 router.get('/test', (req, resp) => {
-    resp.send('This is another test 7');
+    resp.send('This is another test 9');
 });
 
 module.exports = router;
